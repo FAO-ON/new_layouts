d4bfd1c88e1f7b1b8438b11d399a3ba184e1a6a3
Rishabh Chowdhary <rishabh.chowdhary@mail.utoronto.ca>
2023-11-02 13:54:15 -0400

    Changed styles to Grid

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-//test if connected
-console.log("hello world");
+//Display the hamburger menu when clicked
+
+function toggleMenu() {
+    var links = document.getElementById("myLinks");
+    links.style.display = links.style.display === "block" ? "none" : "block";
+}
