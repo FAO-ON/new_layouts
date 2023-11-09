74f73fe1d19fc91882e7d5f6ffc326e74a64359b
Rishabh Chowdhary <rishabh.chowdhary@mail.utoronto.ca>
2023-11-09 11:08:55 -0500

    Search bar takeover built

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function toggleMenu() {
     //toggle the menu
     links.style.display === "block" ? links.style.display = "none" : links.style.display = "block";
 }
+
+function removeMainMenuLinks(){
+    var mainMenuLinks = document.getElementsByClassName("main-menu-links");
+    var top = document.getElementsByClassName("top");
+    var search = 
+    mainMenuLinks[0].style.display = "none";
+    top[0].style.display = "none";
+
+}
+
+function addMainMenuLinks(){
+    var mainMenuLinks = document.getElementsByClassName("main-menu-links");
+    var top = document.getElementsByClassName("top");
+    mainMenuLinks[0].style.display = "flex";
+    top[0].style.display = "flex";
+}
